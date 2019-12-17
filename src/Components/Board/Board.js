db9f76c57970a025250137e8dddeab42a8d49fff
Damon Bolesta <dbolesta@gmail.com>
2019-12-17 01:37:41 -0500

    sets up responsive engraverParams for react-abc, flexbox setup for Card and CardRow

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -13,17 +13,18 @@ import svgs from '../../svgs';
 
 // const notation = 'X:1\nM:C\nL:1\nK:C\nC';
 
+// https://paulrosen.github.io/abcjs/visual/visual-params.html
 const engraverParams = {
   add_classes: false,
   editable: false,
   listener: null,
-  paddingbottom: 30,
-  paddingleft: 15,
-  paddingright: 50,
-  paddingtop: 15,
-  responsive: undefined,
+  paddingbottom: 0,
+  paddingleft: 0,
+  paddingright: 0,
+  paddingtop: 0,
+  responsive: 'resize',
   scale: 1,
-  staffwidth: 740
+  staffwidth: 100
 };
 
 const notation = 'K:clef=none\nL:1\n^c_D';
@@ -32,15 +33,35 @@ const BoardContainer = styled.div`
   border: 1px solid green;
 `;
 
+const CardRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const Card = styled.div`
   border: 1px solid purple;
-  display: inline-block;
   background-color: white;
+  border-radius: 5px;
+  display: inline-flex;
+  flex-direction: column;
+  flex: 0 0 10rem;
+  margin: 0.2rem;
+`;
+
+const Letters = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 50%;
+  margin: auto;
 `;
 
 const FingerImg = styled.img`
-  width: 10rem;
-  padding: 1.25rem;
+  /* width: 10rem; */
+  /* padding: 1.25rem; */
+  padding: 0;
+  width: 100%;
+  /* padding-top: 0; */
 `;
 
 const Staff = styled.div`
@@ -56,23 +77,26 @@ const Board = ({ cards }) => {
     <BoardContainer>
       <p>test board</p>
 
-      {cards.map(card => {
-        return (
-          <Card>
-            {card.letters.map((letter, i) => {
-              return <span>{i !== 0 ? ' / ' + letter : letter}</span>;
-            })}
-            <Notation
-              engraverParams={{
-                ...engraverParams,
-                staffWidth: '100%'
-              }}
-              notation={`K:clef=none\nL:1\n${card.abcCode}`}
-            />
-            <FingerImg src={svgs[card.imgRef]} />
-          </Card>
-        );
-      })}
+      <CardRow>
+        {cards.map(card => {
+          return (
+            <Card>
+              <Letters>
+                {card.letters.map(letter => (
+                  <span>{letter}</span>
+                ))}
+              </Letters>
+              <Notation
+                engraverParams={{
+                  ...engraverParams
+                }}
+                notation={`K:clef=none\nL:1\n${card.abcCode}`}
+              />
+              <FingerImg src={svgs[card.imgRef]} />
+            </Card>
+          );
+        })}
+      </CardRow>
     </BoardContainer>
   );
 };
