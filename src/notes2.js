d467dc84ca394f92123df5276b7344f1690ba516
Damon Bolesta <dbolesta@gmail.com>
2019-12-17 16:18:05 -0500

    adds complete object for each note in notes2.js, allowing for all cards to be displayed with corrent react-abc staff and fingering chart

--- src/notes2.js
+++ src/notes2.js
@@ -2,16 +2,224 @@ const notes = [
   {
     index: 0,
     letters: ['C'],
-    imgSrc: '/Images/0_C.svg',
-    imgRef: 'C',
+    imgRef: 'lowC',
     abcCode: 'C'
   },
   {
     index: 1,
     letters: ['C♯', 'D♭'],
-    imgSrc: '/Images/1_Cs.svg',
-    imgRef: 'Cs',
+    imgRef: 'lowCs',
     abcCode: '^C_D'
+  },
+  {
+    index: 2,
+    letters: ['D'],
+    imgRef: 'lowD',
+    abcCode: 'D'
+  },
+  {
+    index: 3,
+    letters: ['D♯', 'E♭'],
+    imgRef: 'lowDs',
+    abcCode: '^D'
+  },
+  {
+    index: 4,
+    letters: ['E', 'F♭'],
+    imgRef: 'lowMidE',
+    abcCode: 'E_F'
+  },
+  {
+    index: 5,
+    letters: ['E♯', 'F'],
+    imgRef: 'lowMidEs',
+    abcCode: '^EF'
+  },
+  {
+    index: 6,
+    letters: ['F♯', 'G♭'],
+    imgRef: 'lowMidFs',
+    abcCode: '^F_G'
+  },
+  {
+    index: 7,
+    letters: ['G'],
+    imgRef: 'lowMidHiG',
+    abcCode: 'G'
+  },
+  {
+    index: 8,
+    letters: ['G♯', 'A♭'],
+    imgRef: 'lowMidGs',
+    abcCode: '^G_A'
+  },
+  {
+    index: 9,
+    letters: ['A'],
+    imgRef: 'lowMidA',
+    abcCode: 'A'
+  },
+  {
+    index: 10,
+    letters: ['A♯', 'B♭'],
+    imgRef: 'lowMidAs',
+    abcCode: '^A_B'
+  },
+  {
+    index: 11,
+    letters: ['B', 'C♭'],
+    imgRef: 'lowMidB',
+    abcCode: 'B_c'
+  },
+  {
+    index: 12,
+    letters: ['B♯', 'C'],
+    imgRef: 'lowMidBs',
+    abcCode: '^Bc'
+  },
+  {
+    index: 13,
+    letters: ['C♯', 'D♭'],
+    imgRef: 'midHiCs',
+    abcCode: '^c_d'
+  },
+  {
+    index: 14,
+    letters: ['D'],
+    imgRef: 'midD',
+    abcCode: 'd'
+  },
+  {
+    index: 15,
+    letters: ['D♯', 'E♭'],
+    imgRef: 'midDs',
+    abcCode: '^d_e'
+  },
+  {
+    index: 16,
+    letters: ['E', 'F♭'],
+    imgRef: 'lowMidE',
+    abcCode: 'e_f'
+  },
+  {
+    index: 17,
+    letters: ['E♯', 'F'],
+    imgRef: 'lowMidEs',
+    abcCode: '^ef'
+  },
+  {
+    index: 18,
+    letters: ['F♯', 'G♭'],
+    imgRef: 'lowMidFs',
+    abcCode: '^f_g'
+  },
+  {
+    index: 19,
+    letters: ['G'],
+    imgRef: 'lowMidHiG',
+    abcCode: 'g'
+  },
+  {
+    index: 20,
+    letters: ['G♯', 'A♭'],
+    imgRef: 'lowMidGs',
+    abcCode: '^g_a'
+  },
+  {
+    index: 21,
+    letters: ['A'],
+    imgRef: 'lowMidA',
+    abcCode: 'a'
+  },
+  {
+    index: 22,
+    letters: ['A♯', 'B♭'],
+    imgRef: 'lowMidAs',
+    abcCode: '^a_b'
+  },
+  {
+    index: 23,
+    letters: ['B', 'C♭'],
+    imgRef: 'lowMidB',
+    abcCode: "b_c'"
+  },
+  {
+    index: 24,
+    letters: ['B♯', 'C'],
+    imgRef: 'lowMidBs',
+    abcCode: "^bc'"
+  },
+  {
+    index: 25,
+    letters: ['C♯', 'D♭'],
+    imgRef: 'midHiCs',
+    abcCode: "^c'_d'"
+  },
+  {
+    index: 26,
+    letters: ['D'],
+    imgRef: 'hiD',
+    abcCode: "d'"
+  },
+  {
+    index: 27,
+    letters: ['D♯', 'E♭'],
+    imgRef: 'hiDs',
+    abcCode: "^d'_e'"
+  },
+  {
+    index: 28,
+    letters: ['E', 'F♭'],
+    imgRef: 'hiE',
+    abcCode: "e'_f'"
+  },
+  {
+    index: 29,
+    letters: ['E♯', 'F'],
+    imgRef: 'hiEs',
+    abcCode: "^e'f'"
+  },
+  {
+    index: 30,
+    letters: ['F♯', 'G♭'],
+    imgRef: 'hiFs',
+    abcCode: "^f'_g'"
+  },
+  {
+    index: 31,
+    letters: ['G'],
+    imgRef: 'lowMidHiG',
+    abcCode: "g'"
+  },
+  {
+    index: 32,
+    letters: ['G♯', 'A♭'],
+    imgRef: 'hiGs',
+    abcCode: "^g'_a'"
+  },
+  {
+    index: 33,
+    letters: ['A'],
+    imgRef: 'hiA',
+    abcCode: "a'"
+  },
+  {
+    index: 34,
+    letters: ['A♯', 'B♭'],
+    imgRef: 'hiAs',
+    abcCode: "^a'_b'"
+  },
+  {
+    index: 35,
+    letters: ['B', 'C♭'],
+    imgRef: 'hiB',
+    abcCode: "b'_c'"
+  },
+  {
+    index: 36,
+    letters: ['C'],
+    imgRef: 'hiC',
+    abcCode: "c''"
   }
 ];
 
--- src/svgs.js
+++ src/svgs.js
@@ -1,9 +1,57 @@
-import C from './Images/0_C.svg';
-import Cs from './Images/1_Cs.svg';
+import lowC from './Images/0_C.svg';
+import lowCs from './Images/1_Cs.svg';
+import lowD from './Images/2_D.svg';
+import lowDs from './Images/3_Ds.svg';
+import lowMidE from './Images/4-16_E.svg';
+import lowMidEs from './Images/5-17_Es.svg';
+import lowMidFs from './Images/6-18_Fs.svg';
+import lowMidHiG from './Images/7-19-31_G.svg';
+import lowMidGs from './Images/8-20_Gs.svg';
+import lowMidA from './Images/9-21_A.svg';
+import lowMidAs from './Images/10-22_As.svg';
+import lowMidB from './Images/11-23_B.svg';
+import lowMidBs from './Images/12-24_Bs.svg';
+import midHiCs from './Images/13-25_Cs.svg';
+import midD from './Images/14_D.svg';
+import midDs from './Images/15_Ds.svg';
+import hiD from './Images/26_D.svg';
+import hiDs from './Images/27_Ds.svg';
+import hiE from './Images/28_E.svg';
+import hiEs from './Images/29_Es.svg';
+import hiFs from './Images/30_Fs.svg';
+import hiGs from './Images/32_Gs.svg';
+import hiA from './Images/33_A.svg';
+import hiAs from './Images/34_As.svg';
+import hiB from './Images/35_B.svg';
+import hiC from './Images/36_C.svg';
 
 const svgs = {
-  C,
-  Cs
+  lowC,
+  lowCs,
+  lowD,
+  lowDs,
+  lowMidE,
+  lowMidEs,
+  lowMidFs,
+  lowMidHiG,
+  lowMidGs,
+  lowMidA,
+  lowMidAs,
+  lowMidB,
+  lowMidBs,
+  midHiCs,
+  midD,
+  midDs,
+  hiD,
+  hiDs,
+  hiE,
+  hiEs,
+  hiFs,
+  hiGs,
+  hiA,
+  hiAs,
+  hiB,
+  hiC
 };
 
 export default svgs;
