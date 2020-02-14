a4ef0b303a22dc7d0fcfe4cb0a2955310e7d1208
Damon Bolesta <dbolesta@gmail.com>
2020-02-14 14:18:34 -0500

    Updates

---
+++ precache-manifest.180c6bc3c177260131873ff8cc5085c3.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9b6c95966bc0127c8b5f7b3d3e00f2",
+    "url": "/flute-cards/index.html"
+  },
+  {
+    "revision": "6df890d65f51d0a76ad1",
+    "url": "/flute-cards/static/css/2.d2a31a36.chunk.css"
+  },
+  {
+    "revision": "e845d7cade579260edeb",
+    "url": "/flute-cards/static/css/main.d5016946.chunk.css"
+  },
+  {
+    "revision": "6df890d65f51d0a76ad1",
+    "url": "/flute-cards/static/js/2.11ce0fb6.chunk.js"
+  },
+  {
+    "revision": "92ce2a26095b38b79090aaac8821199b",
+    "url": "/flute-cards/static/js/2.11ce0fb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e845d7cade579260edeb",
+    "url": "/flute-cards/static/js/main.43874f2f.chunk.js"
+  },
+  {
+    "revision": "22d388a14d2b0fcb1303",
+    "url": "/flute-cards/static/js/runtime-main.7eb06f2f.js"
+  },
+  {
+    "revision": "6f3257b48089bca31d23ab80dd6fb2b1",
+    "url": "/flute-cards/static/media/0_C.6f3257b4.svg"
+  },
+  {
+    "revision": "5c6f8bb98a5def58db275628b433b4a3",
+    "url": "/flute-cards/static/media/10-22_As.5c6f8bb9.svg"
+  },
+  {
+    "revision": "3a27533a5e32634f5962049c0a8729eb",
+    "url": "/flute-cards/static/media/11-23_B.3a27533a.svg"
+  },
+  {
+    "revision": "5f6714c780e2aee069bb97e0b3a62ecf",
+    "url": "/flute-cards/static/media/12-24_Bs.5f6714c7.svg"
+  },
+  {
+    "revision": "f08284dc3337063b0370de8b409b7258",
+    "url": "/flute-cards/static/media/13-25_Cs.f08284dc.svg"
+  },
+  {
+    "revision": "e8de674e70e27bff2db86e2ea0c9371b",
+    "url": "/flute-cards/static/media/14_D.e8de674e.svg"
+  },
+  {
+    "revision": "8c2696eb754a93e56dd6667d2f66141e",
+    "url": "/flute-cards/static/media/15_Ds.8c2696eb.svg"
+  },
+  {
+    "revision": "ff2e6c9e1c442115f3c1221428466400",
+    "url": "/flute-cards/static/media/1_Cs.ff2e6c9e.svg"
+  },
+  {
+    "revision": "f4ec192278545062d1d7b1ff7d52d5ce",
+    "url": "/flute-cards/static/media/26_D.f4ec1922.svg"
+  },
+  {
+    "revision": "fefc9a89b9b6b37b16439fb3f0f7cc60",
+    "url": "/flute-cards/static/media/27_Ds.fefc9a89.svg"
+  },
+  {
+    "revision": "afff4ef0d1edded97a04678e4635567b",
+    "url": "/flute-cards/static/media/28_E.afff4ef0.svg"
+  },
+  {
+    "revision": "5243e5a46b23764a1c05ad888001a915",
+    "url": "/flute-cards/static/media/29_Es.5243e5a4.svg"
+  },
+  {
+    "revision": "a53ed15c696d228a5ac5dfb27822696a",
+    "url": "/flute-cards/static/media/2_D.a53ed15c.svg"
+  },
+  {
+    "revision": "14c96c6f40ba5d17c3d7be688899d53e",
+    "url": "/flute-cards/static/media/30_Fs.14c96c6f.svg"
+  },
+  {
+    "revision": "d7dc4de6d328ad6ed73e39f32597da1b",
+    "url": "/flute-cards/static/media/31_G.d7dc4de6.svg"
+  },
+  {
+    "revision": "8c730438980677ff3ec7d0fe472374b4",
+    "url": "/flute-cards/static/media/32_Gs.8c730438.svg"
+  },
+  {
+    "revision": "171b6844b5f5293f7cb10ec5c809476c",
+    "url": "/flute-cards/static/media/33_A.171b6844.svg"
+  },
+  {
+    "revision": "b5bebc04cb6082fae6344a78125e4365",
+    "url": "/flute-cards/static/media/34_As.b5bebc04.svg"
+  },
+  {
+    "revision": "5c66cf267412c5726f2aec9b33821f60",
+    "url": "/flute-cards/static/media/35_B.5c66cf26.svg"
+  },
+  {
+    "revision": "fb014568596e4b140bbe943446fb80a8",
+    "url": "/flute-cards/static/media/36_C.fb014568.svg"
+  },
+  {
+    "revision": "a5254f8d42ac058234931f3e18456b72",
+    "url": "/flute-cards/static/media/3_Ds.a5254f8d.svg"
+  },
+  {
+    "revision": "26a3f916e992dc3271738625737c5c66",
+    "url": "/flute-cards/static/media/4-16_E.26a3f916.svg"
+  },
+  {
+    "revision": "3a5547dec880ef6183128a2f9e761046",
+    "url": "/flute-cards/static/media/5-17_Es.3a5547de.svg"
+  },
+  {
+    "revision": "d9726e5c3af073de15f30b83f6235b7e",
+    "url": "/flute-cards/static/media/6-18_Fs.d9726e5c.svg"
+  },
+  {
+    "revision": "3e25e4b13976872615c59f81b3a9df2a",
+    "url": "/flute-cards/static/media/7-19_G.3e25e4b1.svg"
+  },
+  {
+    "revision": "ddded7cfe936edd4112ea8a3ad1f92c3",
+    "url": "/flute-cards/static/media/8-20_Gs.ddded7cf.svg"
+  },
+  {
+    "revision": "df00abe878f6c64d87255ee2885fe43b",
+    "url": "/flute-cards/static/media/9-21_A.df00abe8.svg"
+  },
+  {
+    "revision": "0ec2583d8f978ea12c84962e10098c53",
+    "url": "/flute-cards/static/media/arrows-h.0ec2583d.svg"
+  },
+  {
+    "revision": "aac215623c52c0ba3ec338e2c15234a5",
+    "url": "/flute-cards/static/media/play.aac21562.svg"
+  },
+  {
+    "revision": "48905b5e7a52d91bc54dfbd71e526154",
+    "url": "/flute-cards/static/media/refresh.48905b5e.svg"
+  },
+  {
+    "revision": "fe409d0667f70b0dea5a02e336eacdc8",
+    "url": "/flute-cards/static/media/sharp.fe409d06.svg"
+  },
+  {
+    "revision": "e3f280098995150f5c297209ea5b84aa",
+    "url": "/flute-cards/static/media/step-backward.e3f28009.svg"
+  },
+  {
+    "revision": "c937265a04c949caca0f6a20072fcbd6",
+    "url": "/flute-cards/static/media/trash_white.c937265a.svg"
+  },
+  {
+    "revision": "6cd88816cfcdbf7e1a056ce0cf96c0e5",
+    "url": "/flute-cards/static/media/treble.6cd88816.svg"
+  },
+  {
+    "revision": "4d36e9a578ab43cfdd8bc41a18515e34",
+    "url": "/flute-cards/static/media/wholeNote.4d36e9a5.svg"
+  }
+]);
